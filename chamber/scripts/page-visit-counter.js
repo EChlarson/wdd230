71651bf9b9a132d26073a9b508d8319be6890fa9
EChlarson <chl20001@byui.edu>
2024-06-08 00:06:06 -0500

    discover js

--- chamber/scripts/page-visit-counter.js
+++ chamber/scripts/page-visit-counter.js
@@ -6,7 +6,6 @@ const visitsDisplay = document.querySelector(".visits");
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
 // 3️⃣ Determine if this is the first visit or display the number of visits. 
-// We wrote this example backwards in order for you to think deeply about the logic.
 if (numVisits !== 0) {
 	visitsDisplay.textContent = numVisits;
 } else {
