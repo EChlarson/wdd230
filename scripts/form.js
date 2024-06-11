578faf71f419c2001b275d73d849a0342334667f
EChlarson <chl20001@byui.edu>
2024-06-11 18:11:43 -0500

    form

---
+++ scripts/form.js
@@ -0,0 +1,21 @@
+// Initialize display element variable
+const p1 = document.querySelector("#password");
+const p2 = document.querySelector("#cpassword");
+const message = document.querySelector("#formmessage");
+
+p2.addEventListener("focusout", checkSame);
+
+//Evaluate to see if fields match
+function checkSame() {
+	if (p1.value !== p2.value) {
+		message.textContent = "Passwords do NOT match.";
+		message.style.visibility = "show";
+		p2.style.backgroundColor = "#c30010";
+		p2.value = "";
+		p2.focus();
+	} else {
+		message.style.display = "none";
+		p2.style.backgroundColor = "#fff";
+		p2.style.color = "#000";
+	}
+}
