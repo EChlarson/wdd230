2ce62159e8be639c9b4e035dd7f0abe00567aeba
EChlarson <chl20001@byui.edu>
2024-06-15 15:55:16 -0500

    links.js

--- scripts/links.js
+++ scripts/links.js
@@ -1,12 +1,18 @@
 //Declare Variables
-const baseURL = 'https://echlarson.github.io/wdd230/';
+const card = document.querySelector('#live-links');
+const baseURL = "https://yourgithubusername.github.io/wdd230/";
 const linksURL = 'https://echlarson.github.io/wdd230/data/links.json';
 
 //async function to fetch data from the JSON source url//
-async function getLinks(linksURL) {
+async function getLinks() {
     const response = await fetch(linksURL);
-    const data = await response.json(linksURL);
-    console.log(data.links);
+    const data = await response.json();
+    //console.log(data);
+    displayLinks(data);
   }
   
-  getLinks(linksURL);
+  getLinks();
+
+  const displayLinks = (weeks) => {
+    
+  }
