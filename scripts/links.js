9d2ac0cdc20d88999c26d1cd37b1ef78ddc16880
EChlarson <chl20001@byui.edu>
2024-06-14 23:39:36 -0500

    date to data

--- scripts/links.js
+++ scripts/links.js
@@ -3,10 +3,10 @@ const baseURL = 'https://echlarson.github.io/wdd230/';
 const linksURL = 'https://echlarson.github.io/wdd230/data/links.json';
 
 //async function to fetch data from the JSON source url//
-async function getLinks() {
+async function getLinks(linksURL) {
     const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data);
+    const data = await response.json(linksURL);
+    console.log(data.links);
   }
   
-  getLinks();
+  getLinks(linksURL);
