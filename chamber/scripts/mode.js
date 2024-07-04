48cfa48f05db015a03cbdbb0ee5f30860e7efd0d
EChlarson <chl20001@byui.edu>
2024-07-04 13:36:58 -0500

    mode fix

--- chamber/scripts/mode.js
+++ chamber/scripts/mode.js
@@ -1,14 +1,15 @@
 const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
+const body = document.querySelector("body");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🌔")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
+		body.style.background = "#000";
+		body.style.color = "#fff";
 		modeButton.textContent = "🌒";
+
 	} else {
-		main.style.background = "#eee";
-		main.style.color = "#000";
+		body.style.background = "#eee";
+		body.style.color = "#000";
 		modeButton.textContent = "🌔";
 	}
 });
