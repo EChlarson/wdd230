2a7aac2c9e352a99fd8134527a9bb00eb22ceb66
EChlarson <chl20001@byui.edu>
2024-07-15 22:21:51 -0500

    json fix

--- scootes/scripts/priceChart.js
+++ scootes/scripts/priceChart.js
@@ -1,11 +1,29 @@
 const chart = document.querySelector("#chart");
-const list = 'data/vehicle.json';
+const list = 'https://echlarson.github.io/wdd230/scootes/data/vehicles.json';
 
 async function getCont() {
-    const response = await fetch(contLi);
+    const response = await fetch(list);
     const data = await response.json();
-    //console.log(data);
-    let dataCont = data ['members'];
-    dataCont.forEach(displayGrid);
-    dataCont.forEach(displayList);
+    console.log(data);
+    let dataCont = data ['vehicles'];
+    dataCont.forEach(displayChart);
 }
+
+function displayChart(data) {
+
+    for (let aNum=0; aNum<7; aNum++) {
+        //Build <tr>
+        let tr = document.createElement('tr');
+        let rentalType = document.createElement('td');
+        let maxPersons = document.createElement('td');
+        let resHalf = document.createElement('td');
+        let resFull = document.createElement('td');
+        let walkHalf = document.createElement('td');
+        let walkFull = document.createElement('td');
+
+        rentalType.textContent = `${data.vehicle[aNum].Rental-Type}`;
+        maxPersons.textContent = `${data.vehicle[aNum].Rental-Type}`;
+    
+}}
+
+getCont();
